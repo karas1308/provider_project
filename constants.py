3b9ae8c5c4888f985f01b451cfd24624376e42ae
ihor.shchuka <ihor.shchuka@gmail.com>
2023-11-10 16:49:08 +0200

    added constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+from environment import get_environment
+
+# email_sender
+email_sender = "ishchuka.mail.sender@gmail.com"
+email_password = get_environment("EMAIL_PASSWORD")
+email_receiver = "ishchuka.mail.sender@gmail.com"
+subject = "Notification"
+user_confirm_body = """
+Your balance is {}. Please fund your balance. Services will be suspended in 3 days
+"""
